a025f3b5e38590317b87b695e4a4895214ca9dc2
Nepenthes <sidonie.toulouse@gmail.com>
2018-03-12 16:57:38 +0100

    Add files via upload

---
+++ wikipedia_viewer/script.js
@@ -0,0 +1,102 @@
+window.onload = init;
+
+function init() {
+	const form = document.querySelector('.form form');
+	const randBtn = document.querySelector('.random');
+	const results = document.querySelector('.results');
+	const titre = results.querySelector('.titre');
+	const baseUrl = 'https://en.wikipedia.org/w/api.php';
+	//get result
+	function fetchResults(random, search) {
+		//url parameters
+		//if user search
+		const objet = {
+			action: 'opensearch',
+			search: search,
+			format: 'json',
+			namespace: 0,
+			origin: '*',
+			limit: 12
+		};
+		//if random request
+		const objetRandom = {
+			action: 'query',
+			generator: 'random',
+			format: 'json',
+			origin: '*',
+			titles: 'Main+Page',
+			prop: 'extracts',
+			exlimit: 'max',
+			grnnamespace: 0,
+			exchars: 500 // > 0
+		};
+		if(random === true) {
+			parameters = objetRandom;
+		}else {
+				type: 'GET',
+			parameters = objet;
+		}
+		//request using jquery
+			$.ajax({
+				url: baseUrl,
+				dataType: 'json',
+				data: parameters,
+				success: function(json) {
+					if(random === true) {
+						displayRandom(json);
+					} else {
+						displayWiki(json);
+					}
+				},
+				error: function error() {
+					alert("error");
+				}
+			});
+	}
+	//display results
+	//if user search
+	function displayWiki(json) {
+		//console.log(json)
+		const titles = json[1];
+		const briefs = json[2];
+		const links = json[3];
+		//write title
+		const h1 = json[0];
+		titre.textContent = h1;
+		//build a div per result
+		for( let i = 0, l = titles.length; i < l; i++) {
+			const result = document.createElement('div');
+			result.innerHTML += `<h3>${titles[i]}</h3>
+								<p>${briefs[i]}</p><br>
+								<a href="${links[i]}" target="blank">Read More</a>`;
+			result.classList.add('append');
+			results.appendChild(result);	
+		}
+	}
+	//if random
+	function displayRandom(json) {
+		//console.log(json);
+		const pages = json.query.pages;
+		//create a div
+		const result = document.createElement('div');
+		//use a for in to get to pages.?.ppty because pages.? = an id
+		for(var page in pages) {
+			//build the div
+			const link = `https://en.wikipedia.org/wiki/${pages[page].title}`;
+			result.innerHTML += `<h3>${pages[page].title}</h3>
+								<p>${pages[page].extract}</p><br>
+								<a href="${link}" target="blank">Read More</a>`;
+			result.classList.add('append');
+			results.appendChild(result);	
+		}
+	}
+	//event listeners
+	form.addEventListener('submit', (evt)=> {
+		evt.preventDefault();
+		const userSearch = evt.target.querySelector('[name=keyword').value;
+		fetchResults(false, userSearch); //false: random = false
+	});
+	randBtn.addEventListener('click', ()=> {
+		fetchResults(true);
+	});
+}
