bf521093df740ab3393dd8f3985e3e51df5562a9
Sidonie Rossel <sidonie.toulouse@gmail.com>
2018-10-25 08:58:48 +0200

    resolve date bug

--- wazap_losangeles/time.js
+++ wazap_losangeles/time.js
@@ -58,7 +58,9 @@ const months = ['décembre', 'janvier', 'février', 'mars', 'avril', 'mai', 'jui
 const getDate = stamp => {
   const day = week[new Date(stamp).getDay()];
   const date = new Date(stamp).toLocaleString('fr-FR').split(' ')[0];
-  const month = months[date.split('/')[1].slice(1)];
+    const month = months[date.split('/')[1]].slice(0) === 0 ?
+    months[date.split('/')[1]].slice(1) :
+    months[date.split('/')[1]];
   const dateString = `${day} ${date.split('/')[0]} ${month} ${date.split('/')[2]}`;
   return dateString;
 };
